49d594f9d901d94928a94366371c2770d459526c
Saiful Islam <nysaifulislam@gmail.com>
2022-01-06 20:22:15 -0500

    added GET /api/users/:username/posts route

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dbPool = require('../models');
 const { getAccountInfo } = require('../models/user');
+const { getPostsFromUser } = require('../models/post');
 
 router.get('/:username', async (req, res) => {
     const { username } = req.params;
@@ -14,6 +15,20 @@ router.get('/:username', async (req, res) => {
         return res.status(500).json({ msg: 'Error occurred in the server' });
 
     res.status(200).json(getAccountInfoRes);
+});
+
+router.get('/:username/posts', async (req, res) => {
+    const { username } = req.params;
+    const getPostsFromUserRes = await getPostsFromUser(username, dbPool);
+
+    // username exists (posts or no posts available)
+    if (getPostsFromUserRes[0] || getPostsFromUserRes.length === 0)
+        res.status(200).json(getPostsFromUserRes);
+    // server error
+    else if (getPostsFromUserRes[1] === 'Internal server error')
+        res.status(500).json({ msg: getPostsFromUserRes });
+    // client error (no corresponding username)
+    else res.status(400).json({ msg: getPostsFromUserRes[1] });
 });
 
 module.exports = router;
